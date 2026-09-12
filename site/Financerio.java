a874fa330f05cd7d1a037f08e1999c8516cbe595
LucasKenned <lucaskenned@yahoo.com.br>
2026-09-12 14:54:14 -0300

    feat: adicionado css ao contato e add financeiro

---
+++ site/Financerio.java
@@ -0,0 +1,11 @@
+package site;
+
+public class Financerio {
+    public static void main(String[] args) {
+        String nome = "Lucas";
+        String telefone = "(81) 99999-9999";
+
+        System.out.println("nome: "+nome + "telefone: "+telefone);
+    }
+}
+
